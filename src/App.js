9841dc9f70bc43a5b8350d7adcf11c1b8e69882d
stevanryan <stevanryan85@gmail.com>
2023-04-25 22:21:36 +0700

    adding react query

--- src/App.js
+++ src/App.js
@@ -8,24 +8,35 @@ import Profile from './components/Main/Profile'
 import CartContext from './components/Main/CartContext'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      }
+    }
+  });
+
   return (
     <div className="App">
-      <CartContext>
-        <Router>
-          <Navigation />
-          <Routes>
-            <Route path={'/'} element={<Home />}/>
-            <Route path={'/cart'} element={<Cart/>}></Route>
-            <Route path={'/favourite'} element={<Favourite/>}></Route>
-            <Route path={'/profile'} element={<Profile/>}></Route>
-            <Route path={'*'} element={<h1 style={{paddingTop: '100px'}}>404! NOT FOUND!</h1>}></Route>
-          </Routes>
-        </Router>
-      </CartContext>  
+      <QueryClientProvider client={client}>
+        <CartContext>
+          <Router>
+            <Navigation />
+            <Routes>
+              <Route path={'/'} element={<Home />}/>
+              <Route path={'/cart'} element={<Cart/>}></Route>
+              <Route path={'/favourite'} element={<Favourite/>}></Route>
+              <Route path={'/profile'} element={<Profile/>}></Route>
+              <Route path={'*'} element={<h1 style={{paddingTop: '100px'}}>404! NOT FOUND!</h1>}></Route>
+            </Routes>
+          </Router>
+        </CartContext>
+      </QueryClientProvider>
     </div>
   );
 }
 
-export default App;
+export default App
